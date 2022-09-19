394b68f8855d16137a757301fbb8d7576b66afee
gabiler <lerda.gabriel@gmail.com>
2022-09-18 22:45:18 -0300

    avance pagina burguer

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+require "controlador/ctrPlantilla.php";
+//con esto esta llamando el contenido del archiv ctr plantilla que esta en la carpeta controlador
+
+//creo un objeto llamando la clase creada en ctrplantilla
+$plantilla = new Plantilla();
+
+//aca estoy ejecutando el metodo
+$plantilla->ctrPlantilla();
+
+
+
+
+?>
